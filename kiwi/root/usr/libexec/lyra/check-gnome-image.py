d6610f8f0c42f005d839fb57112eb904914626ad
Rodrigo Brito <183213+britors@users.noreply.github.com>
2026-09-25 08:17:31 -0300

    fix(image): require qualified first-login preparation packages

--- kiwi/root/usr/libexec/lyra/check-gnome-image.py
+++ kiwi/root/usr/libexec/lyra/check-gnome-image.py
@@ -10,11 +10,11 @@ import re
 import subprocess
 
 MINIMUMS = {
-    'vega-gtk': '5.1.44', 'sheliak': '2.0.1', 'lyra-welcome': '0.4.1',
+    'vega-gtk': '5.1.45', 'sheliak': '2.0.1', 'lyra-welcome': '0.4.1',
     'lyra-os-theme': '1.9.3', 'lyra-os-icons': '1.9.4',
     'lyra-nautilus-branding': '1.9.4', 'libreoffice-branding-Lyra': '1.0.0',
     'linuxtoys': '6.9', 'lyra-upgrade': '0.2.3', 'beam': '1.0.1',
-    'sulafat': '1.0.4', 'vegad': '5.1.26',
+    'sulafat': '1.0.4', 'vegad': '5.1.32',
     'lyra-installer': '0.1.0',
     'lyra-downloads': '0.1.3',
     'lyra-downloads-firefox-integration': '0.1.3',
